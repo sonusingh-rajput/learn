20aabaeff21dd82cd4d149792692edc88a737a62
Sonu Singh <sonusinghrajput270@gmail.com>
2024-08-18 16:10:16 +0530

    DataType Conversion Confusion

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-console.log("Hello world!");
-"use strict"; // Treats all JS code as Newer Version
-// alert(5+5); // We are using nodejs , not browser
+// console.log("Hello world!");
+// "use strict"; // Treats all JS code as Newer Version
+// // alert(5+5); // We are using nodejs , not browser
 
-let name = "Sonu Singh";
-let age = 21;
-let isUserLogin = false;
-let state;
-let template = null;
+// let name = "Sonu Singh";
+// let age = 21;
+// let isUserLogin = false;
+// let state;
+// let template = null;
 
-console.log(typeof undefined) // Undefined
-console.log(typeof null) // Object
+// console.log(typeof undefined) // Undefined
+// console.log(typeof null) // Object
 
+
+// 3. DataType Conversion Confusion
+
+let score = 33;
+let score1 = "33";
+let score3 = true;
+let score2 = undefined;
+let score4 = null;
+
+
+console.log(typeof score)
+console.log(typeof score1)
+console.log(Number(score3))
+console.log(Number(score1))
+console.log(Number(score2))
+console.log(Number(score4))
 
