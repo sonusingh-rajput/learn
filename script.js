142c33c73f70d7bae75609ed4b496172f0845708
Sonu Singh <sonusinghrajput270@gmail.com>
2024-08-18 14:34:33 +0530

    Data type

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Hello world!");
+"use strict"; // Treats all JS code as Newer Version
+// alert(5+5); // We are using nodejs , not browser
+
+let name = "Sonu Singh";
+let age = 21;
+let isUserLogin = false;
+let state;
+let template = null;
+
+console.log(typeof undefined) // Undefined
+console.log(typeof null) // Object
+
+
