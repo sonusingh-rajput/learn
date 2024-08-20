3493e2a6c34d3b0d3b83d27ea0ca2d9260953d06
Sonu Singh <sonusinghrajput270@gmail.com>
2024-08-20 08:53:08 +0530

    Comparison of data type

--- script.js
+++ script.js
@@ -14,17 +14,45 @@
 
 // 3. DataType Conversion Confusion
 
-let score = 33;
-let score1 = "33";
-let score3 = true;
-let score2 = undefined;
-let score4 = null;
+// let score = 33;
+// let score1 = "33";
+// let score3 = true;
+// let score2 = undefined;
+// let score4 = null;
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
 
+// console.log(typeof score)
+// console.log(typeof score1)
+// console.log(Number(score3))
+// console.log(Number(score1))
+// console.log(Number(score2))
+// console.log(Number(score4))
 
-console.log(typeof score)
-console.log(typeof score1)
-console.log(Number(score3))
-console.log(Number(score1))
-console.log(Number(score2))
-console.log(Number(score4))
+// Operation 
+// let value = 3;
+// let negValue = -value;
+// console.log(value)
+
+// Comparision of data types
+
+// console.log("2" > 1);
+// console.log("0.2" > 1);
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null === 0)
+// console.log(null >=0)
+// console.log(null <= 0)
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+
+// ===
+
+// console.log("1" === 1)
 
